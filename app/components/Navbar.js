42c4db4f77dcf1c68cfd6ed63b982f76bb1f704b
Oron Paz <oron.paz26@gmail.com>
2024-07-28 19:40:12 +0300

    navbar fixed

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,4 +1,4 @@
-// app/components/Navbar.js
+// components/Navbar.js
 'use client';
 
 import Link from 'next/link';
@@ -17,7 +17,7 @@ const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-800 p-4 relative">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-white font-bold text-xl">
           Your Logo
@@ -36,14 +36,34 @@ const Navbar = () => {
           ))}
         </div>
         <button
-          className="md:hidden text-white"
+          className="md:hidden text-white w-10 h-10 relative focus:outline-none"
           onClick={() => setIsOpen(!isOpen)}
         >
-          {isOpen ? 'Close' : 'Menu'}
+          <span className="sr-only">Open main menu</span>
+          <div className="block w-5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <span
+              aria-hidden="true"
+              className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                isOpen ? 'rotate-45' : '-translate-y-1.5'
+              }`}
+            ></span>
+            <span
+              aria-hidden="true"
+              className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                isOpen ? 'opacity-0' : 'opacity-100'
+              }`}
+            ></span>
+            <span
+              aria-hidden="true"
+              className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                isOpen ? '-rotate-45' : 'translate-y-1.5'
+              }`}
+            ></span>
+          </div>
         </button>
       </div>
       {isOpen && (
-        <div className="md:hidden">
+        <div className="md:hidden absolute bg-gray-800 w-screen left-0 top-16 px-4">
           {navItems.map((item) => (
             <Link
               key={item.path}
