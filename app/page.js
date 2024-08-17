af1741356bbcccf09c25ab65cff1cc4321308f5b
Oron Paz <oron.paz26@gmail.com>
2024-08-16 18:49:06 -0700

    cssed up the main page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,10 @@
 import React, { useEffect } from 'react';
 import Image from 'next/image';
 import picture from '../public/avatar/defaultAvatar.png';
+import cassetteImage from '../public/site/music-can.webp';
+import retroComp from '../public/site/retroComp.png';
+import gameboyImage from '../public/site/Game-Boy-FL.png';
+import recordPlayer from '../public/site/recordPLayer.png';
 
 export default function Home() {
   useEffect(() => {
@@ -26,12 +30,16 @@ export default function Home() {
       </div>
   
       {/* Main content */}
-      <div className="relative z-10">
+      <div className="relative z-0">
         {/* Hero section with floating objects */}
         <div className="flex flex-col items-start justify-center min-h-screen px-8 bg-white bg-opacity-80">
-          <h1 className="text-9xl font-bold text-yellow-500 mb-20 mt-6 leading-none bottom-20">
+          <h1 className="text-9xl font-bold text-yellow-500 mb-20 mt-16 leading-none bottom-20 ml-7">
             ORONS<br />RETRO<br />SHOP
           </h1>
+          {/* Cassette image */}
+          <div className="absolute top-0 right-10 mt-4">
+            <Image src={cassetteImage} alt="Retro Cassette" className="w-[50vw] h-auto" />
+          </div>
           <div className="max-w-2xl mt-16 self-center">
             <p className="text-center text-3xl text-stone-800">
               A unique retro store for any tech enthusiast or collector.
@@ -41,17 +49,20 @@ export default function Home() {
           </div>
   
           {/* Floating objects */}
-          <div className="floating-object absolute top-1/4 left-1/4 w-32 h-32">
-            <Image src={picture} layout="fill" objectFit="contain" alt="Retro item 1" />
+          <div className="floating-object absolute top-1/2 left-24 w-32 h-32 ">
+            <Image src={gameboyImage} layout="fill" objectFit="contain" alt="Retro item 1" />
           </div>
-          <div className="floating-object absolute top-1/4 right-1/4 w-40 h-40">
-            <Image src={picture} layout="fill" objectFit="contain" alt="Retro item 2" />
+          <div className="floating-object absolute bottom-40 right-80 w-40 h-40 ">
+            <Image src={retroComp} layout="fill" objectFit="contain" alt="Retro item 2" />
+          </div>
+          <div className="floating-object absolute bottom-1/2 right-40 w-32 h-32 ">
+            <Image src={recordPlayer} layout="fill" objectFit="contain" alt="Retro item 2" />
           </div>
           {/* Add more floating objects as needed */}
         </div>
   
         {/* About section */}
-        <div className="min-h-screen flex items-center justify-center px-8 py-16 bg-stone-100 bg-opacity-90 mt-24">
+        <div className="min-h-screen flex items-center justify-center px-8 py-16 bg-stone-100 bg-opacity-90 mt-24 z-10">
           <div className="max-w-4xl">
             <h2 className="text-4xl font-bold mb-8 text-center text-stone-800">About Us</h2>
             <p className="text-xl text-stone-700 mb-6">
