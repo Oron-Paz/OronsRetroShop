7f6e6cf0003b9feae11ae0360e6c0386b6881038
Oron Paz <oron.paz26@gmail.com>
2024-08-29 08:30:45 -0700

    added some stylingt to cart page

--- app/cart/page.js
+++ app/cart/page.js
@@ -4,6 +4,8 @@ import { useCart } from '../hooks/useCart.js';
 import { useAuth } from '../hooks/useAuth';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import CartImage from '../../public/site/emptyCart.png';
+import Link from 'next/link';
 import Image from 'next/image';
 
 export default function CartPage() {
@@ -42,7 +44,13 @@ export default function CartPage() {
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8">Your Cart</h1>
       {cart.length === 0 ? (
-        <p>Your cart is empty.</p>
+        <div>
+         <p class="text-gray-700 text-lg">Your cart is empty.</p>
+          <p class="text-gray-700 text-lg">
+          It's lonely here, why don't we go to the <Link href="/shop" class="text-yellow-600 underline">shop</Link> to add some items.
+          </p>
+          <Image src={CartImage} alt="Empty Cart" width={300} height={300} className="mx-auto block filter grayscale"/>
+        </div>
       ) : (
         <>
           {cart.map((item) => (
